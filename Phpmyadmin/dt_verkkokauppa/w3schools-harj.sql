8c9c2580444f130a91d13e946705c351859ead43
MoonGraves <117527989+MoonGraves@users.noreply.github.com>
2025-05-26 20:07:06 +0300

    Update w3schools-harj.sql

--- Phpmyadmin/dt_verkkokauppa/w3schools-harj.sql
+++ Phpmyadmin/dt_verkkokauppa/w3schools-harj.sql
@@ -297,6 +297,32 @@ VALUES ('Gilbert Tate', 23634.87, 'gilbert@example.com', 'Australia', '1996-3-04
 INSERT INTO Asiakkaat (nimi, sähköposti, osoite, saldo) 
 VALUES ('Nicole Colombi', 'nicole@example.com', 'Italy', 9999.12);
 
+
+-- lisätään 5 uutta henkilöä "Asiakkaan" taulukkoon - käytettään "INSERT INTO" - KOMENTOA, yksi aikaisempi testausta, et miten lisätään käyttäjä ja saldosta n. siltä väliltä
+INSERT INTO Asiakkaat (nimi, sähköposti, osoite, rekisteröitymis_pvm, saldo) VALUES 
+('William Taylor', 'william@example.com', 'Manchester, UK', '2025-05-22', ROUND(RAND() * 50000, 2)),
+('Sophia Garcia', 'sophia@example.com', 'Madrid, Spain', '2023-07-15', ROUND(RAND() * 50000, 2)),
+('James Anderson', 'james@example.com', 'Berlin, Germany', '2024-10-30', ROUND(RAND() * 50000, 2)),
+('Olivia White', 'olivia@example.com', 'Paris, France', '2022-12-05', ROUND(RAND() * 50000, 2)),
+('Daniel Lee', 'daniel@example.com', 'Tokyo, Japan', '2023-03-18', ROUND(RAND() * 50000, 2));
+
+
+-- yksittäisen käyttäjän lisääminen ilman satunnaista lukua, et syöttää saldon itse
+INSERT INTO Asiakkaat (nimi, sähköposti, osoite, rekisteröitymis_pvm, saldo) VALUES 
+('Harry Shadows', 'harry@example.com', 'Newcastle, UK', '2005-07-13', 9999.00);
+
+
+-- 5 henkilöä lisää ja rekisteröiymis pv n. 2000 - 2025 väliltä
+INSERT INTO Asiakkaat (nimi, sähköposti, osoite, rekisteröitymis_pvm, saldo) VALUES 
+('Nathan Reed', 'nathan@example.com', 'Dallas, USA', DATE_SUB('2025-12-31', INTERVAL FLOOR(RAND() * 9490) DAY), ROUND(RAND() * 50000, 2)),
+('Isabella Evans', 'isabella@example.com', 'Auckland, New Zealand', DATE_SUB('2025-12-31', INTERVAL FLOOR(RAND() * 9490) DAY), ROUND(RAND() * 50000, 2)),
+('Ethan Hall', 'ethan@example.com', 'Toronto, Canada', DATE_SUB('2025-12-31', INTERVAL FLOOR(RAND() * 9490) DAY), ROUND(RAND() * 50000, 2)),
+('Lily Adams', 'lily@example.com', 'Berlin, Germany', DATE_SUB('2025-12-31', INTERVAL FLOOR(RAND() * 9490) DAY), ROUND(RAND() * 50000, 2)),
+('Jackson Murphy', 'jackson@example.com', 'Sydney, Australia', DATE_SUB('2025-12-31', INTERVAL FLOOR(RAND() * 9490) DAY), ROUND(RAND() * 50000, 2));
+
+
+
+
 --=================================================================================
 -- Test for NULL Values
 
@@ -437,6 +463,12 @@ SET saldo = ROUND(RAND() * 50000, 2);
 -- päivitettään asiakkaiden saldo lisäämällä 10% jos saldo on alle 5000
 UPDATE Asiakkaat SET saldo = saldo * 1.1 
 WHERE saldo < 5000;
+
+
+-- päivitystä rekisteröitymis_pvm _ satunnaisesti n. 1950 - 2010
+-- aloituspiste on 2010-12-31 josta päivämääriä vähennettään randomisti 21915 päivää - vastaa n. 60 vuotta.
+UPDATE Asiakkaat
+SET rekisteröitymis_pvm = DATE_SUB('2010-12-31', INTERVAL FLOOR(RAND() * 21915) DAY);
 
 
 -- ===============================================
